c409f2495ff50189d9313c255fdc7df34c52b967
Seu nome <cnmarcos00@gmail.com>
2025-02-19 21:15:03 -0300

    created styled and componentof history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,60 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History() {
-  return <h1>history</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>28 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
